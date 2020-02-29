b7dc49d2aae5030bc0ea80d4e07d79ba669dc1e7
Chris McFarland <cmcfarla@eng.ucsd.edu>
2020-02-29 13:40:10 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+SOURCES := $(wildcard src/*.cpp)
+HEADERS := $(wildcard include/*.h)
+OBJECTS := $(SOURCES:src/%.cpp=build/%.o)
+
+INCLUDE_GLFW := $(shell pkg-config --cflags glfw3)
+LINK_GLFW := $(shell pkg-config --cflags --libs --static glfw3) -lGL
+
+.PHONY: clean
+
+a.out : $(OBJECTS) Makefile
+	@echo "Linking $@"
+	@g++ -o $@ $(OBJECTS) $(LINK_GLFW) -ljpeg
+
+build/%.o : src/%.cpp $(HEADERS) Makefile
+	@echo "Compiling: $<"
+	@mkdir -p ./build/
+	@g++ -Og -std=c++11 -o $@ -c $< -Iinclude $(INCLUDE_GLFW)
+
+clean:
+	@rm -rf ./build/
+	@rm -f ./a.out
+
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <cmath>
+#include <cstdio>
+#include <random>
+using namespace std;
+
+struct Vec3
+{
+    double x;
+    double y;
+    double z;
+    
+    Vec3() : x(0), y(0), z(0) {}
+    Vec3(double X, double Y, double Z) : x(X), y(Y), z(Z) {}
+};
+
+struct LatLong
+{
+    double lat;
+    double long_;
+    
+    LatLong() : lat(0), long_(0) {}
+    LatLong(double LAT, double LONG) : lat(LAT), long_(LONG) {}
+};
+
+Vec3 latlong_to_vec3(const LatLong& LL)
+{
+    Vec3 result;
+    
+    result.x = cos(LL.long_) * cos(LL.lat);
+    result.y = sin(LL.long_) * cos(LL.lat);
+    result.z = sin(LL.lat);
+    
+    return result;
+}
+
+LatLong vec3_to_latlong(const Vec3& v)
+{
+    LatLong result;
+    result.long_ = atan2(v.y, v.x);
+    while(result.long_ < 0)
+        result.long_ += 2*M_PI;
+        
+    result.lat = asin(v.z);
+    
+    return result;
+}
+
+double deg2rad(double deg)
+{
+    return M_PI / 180.0 * deg;
+}
+
+double rad2deg(double rad)
+{
+    return 180.0 / M_PI * rad;
+}
+
+bool operator==(const LatLong& a, const LatLong& b)
+{
+    const double EPS = 0.000001;
+    return fabs(a.lat - b.lat) < EPS && fabs(a.long_ - b.long_) < EPS;
+}
+
+bool operator!=(const LatLong& a, const LatLong& b)
+{
+    return !(a == b);
+}
+
+
+int main()
+{
+    std::mt19937 rng;
+    std::uniform_real_distribution<double> random_turn(0, 2*M_PI);
+    std::uniform_real_distribution<double> random_vertical(-M_PI/2, M_PI/2);
+
+    for(size_t i = 0; i < 1024*1024*1024; i++)
+    {
+        LatLong LL(random_vertical(rng), random_turn(rng));
+        Vec3 v = latlong_to_vec3(LL);
+        LatLong LL2 = vec3_to_latlong(v);
+        
+        if(LL != LL2)
+        {
+            printf("BAD: %f %f != %f %f\n", LL.lat, LL.long_, LL2.lat, LL2.long_);
+            break;
+        }
+    }
+    
+    return 0;
+}
+
